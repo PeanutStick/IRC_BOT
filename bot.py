b41898a1ed1b915c3d80f054db92e150084e5445
PeanutStick <38986405+PeanutStick@users.noreply.github.com>
2021-04-18 18:56:43 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,31 +4,48 @@ import socket, ssl
 # Some basic variables used to configure the bot
 server = 'irc.evilcorp.ga' # Server
 port = 6697 # Port
-channel = "#lab" # Channel
-botnick = "yournick" # Your bots nick
+channel = "#lobby" # Channel
+botnick = "Dovahkiin" # Your bots nick
 user = botnick + " " + botnick + " " + botnick + " " + "a simple irc bot written in python" #This is username, hostname, identity and description in the order
 
 def ircsend(msg): #I had to make another function for send() because in python3 and above the socket incoming and outgoing messages are in bytes format. So you have to encode and decode it accordingly.
-	ircsock.send(msg.encode('utf-8'))
+        ircsock.send(msg.encode('utf-8'))
 
 def ping(ircbuff): # This is our first function! It will respond to server Pings.
-  pongReply = ircbuff.split(':',1)[-1]	#This strips out ping reply and server name for pong. If not defined it can cause the bot to break in some servers
-  ircsend("PONG :" + pongReply + '\r\n')
+        pongReply = ircbuff.split(':',1)[-1]	#This strips out ping reply and server name for pong. If not defined it can cause the bot to break in some servers
+        ircsend("PONG :" + pongReply + '\r\n')
  
 def sendmsg(chan , msg): # This is the send message function, it simply sends messages to the channel.
-  ircsend("PRIVMSG "+ chan +" :"+ msg +"\r\n")
+        ircsend("PRIVMSG "+ chan +" :"+ msg +"\r\n")
  
 def joinchan(chan): # This function is used to join channels.
-  ircsend("JOIN "+ chan +"\r\n")
+        ircsend("JOIN "+ chan +"\r\n")
  
 def hello(): # This function responds to a user that inputs "Hello Mybot"
-  ircsend("PRIVMSG "+ channel +" :Hello!\r\n")
+        ircsend("PRIVMSG "+ channel +" :Hello!\r\n")
 def coffee(): # This function responds to a user that inputs "Hello Mybot"
-  ircsend("PRIVMSG "+ channel +" :  ,--.\r\n")
-  ircsend("PRIVMSG "+ channel +" : C|<3| Here is your coffee !\r\n")
-  ircsend("PRIVMSG "+ channel +" :  `=='\r\n")
+        ircsend("PRIVMSG "+ channel +" :  ,--.\r\n")
+        ircsend("PRIVMSG "+ channel +" : C|<3| Here is your coffee !\r\n")
+        ircsend("PRIVMSG "+ channel +" :  `=='\r\n")
   
-
+def yt_title(buff): # This function responds to a user that inputs "Hello Mybot"
+        import yt_title as title
+        buff = buff.split("watch?v=")
+        buff = buff[1]
+        try:
+                buff = buff.split("&list")
+        except:
+                print("not a list")
+        try:
+                buff = buff.split("?t=")
+        except:
+                print("not shared with time code")
+        
+        ytidd=buff[0] #I should grep btw "watch?v=" and if isset "&list" or "?t="
+        
+        
+        #ytidd="dQw4w9WgXcQ"
+        ircsend("PRIVMSG "+ channel +" :Title: "+title.main(ytidd)+"\r\n")
 
 socketHandler = socket.socket(socket.AF_INET, socket.SOCK_STREAM)		#Opening up a normal socket.
 
@@ -57,12 +74,12 @@ while 1: # Be careful with these! it might send you to an infinite loop
     if ircbuff.find("NOTICE " + botnick + " :*** You are connected to") != -1: #If connected successfully, do the below functions and codes.
         joinchan(channel) # Join the channel using the functions we previously defined
 
-    if ircbuff.find(":Hello "+ botnick) != -1: # If we can find "Hello Mybot" it will call the function hello()
-        hello()
+    #if ircbuff.find(":Hello "+ botnick) != -1: # If we can find "Hello Mybot" it will call the function hello()
+    #    hello()
     if ircbuff.find("coffee") != -1: # Bring you a coffee
-            coffee()
-    if ircbuff.find("https://") != -1: #https://www.youtube.com/watch?v=pxcI5g2iUCg
-            coffee()
-
+        coffee()
+    if ircbuff.find("watch?v=") != -1: #https://www.youtube.com/watch?v=pxcI5g2iUCg
+        yt_title(ircbuff)
+        
 
     del ircbuff	#This will clear out the server incoming buffer so that it can be reused for upcoming buffer.
