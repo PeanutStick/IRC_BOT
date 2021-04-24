2ba08ce209ef51b319c7d1fe01d8ca53a1e85ca7
PeanutStick <38986405+PeanutStick@users.noreply.github.com>
2021-04-24 16:47:05 +0200

    Update yt_title.py

--- yt_title.py
+++ yt_title.py
@@ -13,18 +13,15 @@ from oauth2client import tools
 from oauth2client.file import Storage 
 import json
 scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
-ytidd="dQw4w9WgXcQ"
+#ytidd="dQw4w9WgXcQ"
 def main(id):
    
     # Disable OAuthlib's HTTPS verification when running locally.
     # *DO NOT* leave this option enabled in production.
     os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
-
     api_service_name = "youtube"
     api_version = "v3"
     client_secrets_file = "YOUR_CLIENT_SECRET_FILE.json"
-
-
     # Get credentials and create an API client
     credential_path = os.path.join('./', 'credential_sample.json')
     store = Storage(credential_path)
@@ -32,18 +29,15 @@ def main(id):
     if not credentials or credentials.invalid:#to store the creds and don't have to login again
         flow = client.flow_from_clientsecrets(client_secrets_file,scopes)
         credentials = tools.run_flow(flow, store)
-        
     youtube = googleapiclient.discovery.build(
         api_service_name, api_version, credentials=credentials) #You can find this in the doc
     request = youtube.videos().list(
-        part="snippet", #I only get the snipet
- 
+        part="snippet", #I only get the snipet 
         id=id #id of YT video, I will give it via an external variable from bot.py
     )
     response = request.execute()
     items = response['items'][0] #take item from the dict and become a list if I don't select the first element
     title = items["snippet"]["title"] #I take the title from the snippet item
-    print(title)
     return title
 
 if __name__ == "__main__":
