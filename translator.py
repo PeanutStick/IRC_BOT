1c71aa6dfc24d5039da06bc526c484eecfb9dd70
PeanutStick <38986405+PeanutStick@users.noreply.github.com>
2021-04-27 09:58:31 +0200

    Update translator.py
    I am using 2 detectors to see if the tect is in English.   
    Because it was translating all texts to English and I was blocked by google translate.   
    I also remove akuma member names and don't translate some emoji with text in China or Japan.

--- translator.py
+++ translator.py
@@ -1,7 +1,26 @@
 from googletrans import Translator
+from langdetect import detect
+from textblob import TextBlob
+
+
+
 def main(text):
-    translator = Translator()
-    result = translator.translate(text, dest='en')
-    return result
+    text = text.lower()
+    if "]:" in text:# because we work with akuma, and he translate psedos from discord
+        text = text.split("]:")[1]# I have a problem like this [bleu]: ¯\_(ツ)_/¯
+        
+    test_list = ['q(≧▽≦q)', '¯\_(ツ)_/¯','ω',"Σ","д","Д"]# To see if it's an emoji
+    emoji = [ele for ele in test_list if(ele in text)]# he don't have to disable the translator for this, I should remoove the emoji and add it again
+    
+
+    lang = detect(text)
+    b = TextBlob(text)
+    textblob = b.detect_language()
+    if lang != "en" and textblob!= "en" and bool(emoji)!=1: # I use 2 detectors, if one of them is "en" I don't translate, I also don't translate emoji. We heve too much weebs 
+        translator = Translator()
+        result = translator.translate(text, dest='en').text# I pick the translated text 
+        if result != text: # If the translated word is not the same as the original
+            return result
+
 if __name__ == "__main__":
     main(text)
